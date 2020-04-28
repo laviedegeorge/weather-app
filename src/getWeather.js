82c577c2433c0855bb49e4989c1bcf4bd32678c9
laviedegeorge <laviedegeorge1292@gmail.com>
2020-04-28 23:11:30 +0100

    added the get weather js forgetting the info. for a location requested

---
+++ src/getWeather.js
@@ -0,0 +1,27 @@
+const API_KEY = "b8aa6b77e85dc3ac3f6db7694ca0e9ea";
+
+const getCityData = (city = '', lat = 9.08 , long = 8.68) => {
+  let url 
+  if (city !== '') {
+    url = `//api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  } else {
+    url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
+  }
+  console.log(url)
+  return new Promise((res, err) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(res)
+      .catch(err);
+  });
+};
+
+const getCityInfo = (city, displayData, lat, long,) => {
+  getCityData(city, lat, long)
+    .then(displayData)
+    .catch((err) => {
+      console.log(err,"City not Found!!!");
+    });
+};
+
+export default getCityInfo;
